4fe6e4128afe11de27b290c57ba9e3a9bba20dab
Osama Dahnoun <omdahnou@ncsu.edu>
2022-04-20 03:21:40 -0400

    show menu scanner and run method on new object

--- src/Bank.java
+++
@@ -1,4 +0,0 @@
-public class Bank {
-    Account osama = new Account("Osama Dahnoun", "924");
-
-}
---
+++ src/OsamaBank/OsamaBank.java
@@ -0,0 +1,11 @@
+package OsamaBank;
+
+public class OsamaBank {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        Account osama = new Account("Osama Dahnoun", "924");
+        osama.showMenu();
+    }
+
+}
